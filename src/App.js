98f39b605406f3cd6c7fe210830eae87cf428857
abhishek421 <abhishek0art@gmail.com>
2021-07-19 21:17:21 +0530

    2 files modified, 3 deleted

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import { useState,useEffect,useContext} from 'react';
-import {UserContext} from './userContext';
-import Home from './Home';
-import Auth from './Auth';
+import React, { useState } from 'react';
+import './style.css';
+import Card from './Components/Card';
+import AddBtn from './Components/AddBtn';
 
-function App() {
-  const [user,setUser] = useContext(UserContext);
+export default function App() {
+  const [data, setData] = useState([]);
 
   return (
-      <div>
-      {
-        user?
-        <Home />
-        :
-        <Auth />
-      }
+    <div className="flex flex-col md:flex-wrap items-center h-screen p-4 w-full md:bg-gray-200">
+      <AddBtn data={data} setData={setData} />
+      <div className="flex flex-col w-full md:w-1/3 md:bg-white md:shadow-2xl md:border-gray-800 md:rounded-xl md:px-4 md:overflow-scroll md:h-full">
+        <p className="text-2xl text-center font-semibold sticky top-0 bg-white p-4">
+          Tasks
+        </p>
+        {data.length ? (
+          data.map(item => (
+            <Card title={item.title} due={item.due} brand={item.brand} />
+          ))
+        ) : (
+          <div className="w-full h-full grid place-items-center text-gray-400 font-semibold text-xl">
+            No Tasks Available{' '}
+          </div>
+        )}
       </div>
-  )
+    </div>
+  );
 }
-
-export default App
--- src/Components/AddBtn.js
+++ src/Components/AddBtn.js
@@ -1,23 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Popover } from '@headlessui/react';
 
-export default function AddBtn() {
+export default function AddBtn({ data, setData }) {
+  const [ipTitle, setTitle] = useState('');
+  const [ipDue, setDue] = useState('');
+  const [ipBrand, setBrand] = useState('');
+  const addTask = () => {
+    let newData = {
+      title: ipTitle,
+      due: ipDue,
+      brand: ipBrand
+    };
+    setData(data => [...data, newData]);
+  };
   return (
     <Popover className="fixed bottom-10 right-10 z-10">
       {({ open }) => (
         <>
-          <Popover.Panel className="fixed top-0 inset-0 py-24 px-10 md:px-40 w-full h-full z-10 bg-gray-800 bg-opacity-50">
-            <div className="flex flex-col shadow-xl h-full rounded-xl border-4 border-gray-300 bg-white p-3">
-              <p className="text-2xl font-bold self-center">Add Task</p>
+          <Popover.Panel className="fixed top-0 inset-0 py-24 px-10 md:px-80 w-full h-full z-10 bg-gray-800 bg-opacity-50">
+            <div className="flex flex-col shadow-xl h-full justify-evenly rounded-xl border-4 border-gray-300 gap-4 bg-white p-3">
+              <p className="text-2xl font-bold self-center">
+                Add Task to Board
+              </p>
+              <div className="w-full">
+                <p>Title</p>
+                <input
+                  type="text"
+                  className="w-full p-2 bg-gray-300 rounded-xl"
+                  value={ipTitle}
+                  onChange={e => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="w-full">
+                <p>Due Date</p>
+                <input
+                  type="date"
+                  className="w-full p-2 bg-gray-300 rounded-xl"
+                  value={ipDue}
+                  onChange={e => setDue(e.target.value)}
+                />
+              </div>
+              <div className="w-full">
+                <p>Brand</p>
+                <input
+                  type="text"
+                  className="w-full p-2 bg-gray-300 rounded-xl"
+                  value={ipBrand}
+                  onChange={e => setBrand(e.target.value)}
+                />
+              </div>
+              <button
+                onClick={addTask}
+                className="p-2 rounded-xl bg-purple-600 text-white font-bold text-2xl"
+              >
+                Submit
+              </button>
             </div>
-
-            <img src="/solutions.jpg" alt="" />
           </Popover.Panel>
           <Popover.Button
             className={`absolute bottom-0 right-0 z-10 p-2 rounded-lg font-bold text-white rounded-full inline-block border-2 border-gray-600 bg-gray-400 transition duration-100 ease-in-out transform ${
               open ? 'rotate-45' : ''
             }`}
-          > 
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-8 w-8"
